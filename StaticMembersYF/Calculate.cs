6410be5e88b779f07d852a233d3c5eae4fa961e0
Yonayv <fixicoyonayv@gmail.com>
2023-04-27 12:55:48 -0500

    Initial commit

---
+++ StaticMembersYF/Calculate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembersYF
+{
+    static class Calculate
+    {
+
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        // INT
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        // FLOATING
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
